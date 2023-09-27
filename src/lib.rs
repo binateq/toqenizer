2bacfba11f9bfa50796366f2bb5c29380223324d
Mark Shevchenko <markshevchenko@gmail.com>
2023-09-28 02:15:30 +0300

    Fix implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,69 @@
-use std::ops;
+use std::ops::{BitOr, Shr, Range};
 
 type Predicate = fn(char) -> bool;
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum TokenBuilder<'a> {
+pub enum Element<'a> {
     Char(char),
     Predicate(Predicate),
     String(&'a str),
-    Repeat(Box<TokenBuilder<'a>>, usize, Option<usize>),
-    And(Box<TokenBuilder<'a>>, Box<TokenBuilder<'a>>),
-    Or(Box<TokenBuilder<'a>>, Box<TokenBuilder<'a>>),
+    Repeat(Box<Element<'a>>, Range<u32>),
+    Sequence(Vec<Box<Element<'a>>>),
+    Or(Box<Element<'a>>, Box<Element<'a>>),
     Eof
     // TODO: Skip
     // TODO: Case Insensitive
     // TODO: Convert strings like \x0A to '\n' or \u0041 to 'A'
 }
 
-pub trait Tok<'a> {
-    fn tok(self) -> TokenBuilder<'a>;
+pub trait Elem<'a> {
+    fn elem(self) -> Element<'a>;
 }
 
-impl<'a> Tok<'a> for char {
-    fn tok(self) -> TokenBuilder<'a> {
-        TokenBuilder::Char(self)
+impl<'a> Elem<'a> for char {
+    fn elem(self) -> Element<'a> {
+        Element::Char(self)
     }
 }
 
-impl<'a> Tok<'a> for &'a str {
-    fn tok(self) -> TokenBuilder<'a> {
-        TokenBuilder::String(self)
+impl<'a> Elem<'a> for &'a str {
+    fn elem(self) -> Element<'a> {
+        Element::String(self)
+    }
+}
+
+impl<'a> Elem<'a> for Vec<Element<'a>> {
+    fn elem(self) -> Element<'a> {
+        let mut vector = Vec::new();
+
+        for element in self.into_iter() {
+            vector.push(Box::new(element))
+        }
+
+        Element::Sequence(vector)
+    }
+}
+
+#[cfg(test)]
+mod elem_should {
+    use super::{Element, Elem};
+
+    #[test]
+    fn make_char_element() {
+        assert_eq!(Element::Char('a'), 'a'.elem());
+    }
+
+    #[test]
+    fn make_string_element() {
+        assert_eq!(Element::String("foo"), "foo".elem());
+    }
+
+    #[test]
+    fn make_sequence_element() {
+        let expected = Element::Sequence(vec![
+            Box::new(Element::Char('a')),
+            Box::new(Element::String("foo"))]);
+        assert_eq!(expected, vec!['a'.elem(), "foo".elem()].elem());
     }
 }
 
@@ -39,93 +74,81 @@ impl<'a> Tok<'a> for &'a str {
 //     }
 // }
 
-pub fn p(predicate: Predicate) -> TokenBuilder<'static> {
-    TokenBuilder::Predicate(predicate)
+pub fn p(predicate: Predicate) -> Element<'static> {
+    Element::Predicate(predicate)
 }
 
-impl<'a> TokenBuilder<'a> {
-    pub fn rep(self, min: usize, max: Option<usize>) -> Self {
-        if let Some(max) = max {
-            if min > max {
-                panic!("regex_repeat: min parameters can't be greater than max")
-            }
-        }
-        
-        TokenBuilder::Repeat(Box::new(self), min, max)
+impl<'a> Element<'a> {
+    pub fn rep(self, range: Range<u32>) -> Self {
+        Element::Repeat(Box::new(self), range)
     }
 
     pub fn rep0(self) -> Self {
-        self.rep(0, None)
+        self.rep(0..u32::MAX)
     }
 
     pub fn rep1(self) -> Self {
-        self.rep(1, None)
+        self.rep(1..u32::MAX)
     }
 
     pub fn opt(self) -> Self {
-        self.rep(0, Some(1))
+        self.rep(0..1)
     }
 }
 
 #[cfg(test)]
 mod regex_should {
-    use super::{TokenBuilder, Tok};
+    use super::{Element, Elem};
 
     #[test]
     fn make_repeat_3_4_when_rep_min_is_3_and_max_is_4() {
-        let actual = "abc".tok().rep(3, Some(4));
-        let expected = TokenBuilder::Repeat(Box::new(TokenBuilder::String("abc")), 3, Some(4));
+        let actual = "abc".elem().rep(3..4);
+        let expected = Element::Repeat(Box::new(Element::String("abc")), 3..4);
 
         assert_eq!(expected, actual);
     }
 
     
     #[test]
-    #[should_panic]
-    fn panic_when_rep_min_is_4_and_max_is_3() {
-        let _ = "abc".tok().rep(4, Some(3));
+    fn make_repeat_4_3_when_rep_min_is_4_and_max_is_3() {
+        let actual = "abc".elem().rep(4..3);
+        let expected = Element::Repeat(Box::new(Element::String("abc")), 4..3);
+
+        assert_eq!(expected, actual);
     }
 
     #[test]
     fn make_repeat_0_1_when_opt() {
-        let actual = 'a'.tok().opt();
-        let expected = TokenBuilder::Repeat(Box::new(TokenBuilder::Char('a')), 0, Some(1));
+        let actual = 'a'.elem().opt();
+        let expected = Element::Repeat(Box::new(Element::Char('a')), 0..1);
 
         assert_eq!(expected, actual);
     }
 }
 
-impl<'a> ops::BitAnd<TokenBuilder<'a>> for TokenBuilder<'a> {
-    type Output = TokenBuilder<'a>;
+impl<'a> BitOr<Element<'a>> for Element<'a> {
+    type Output = Element<'a>;
 
-    fn bitand(self, rhs: TokenBuilder<'a>) -> TokenBuilder<'a> {
-        TokenBuilder::And(Box::new(self), Box::new(rhs))
-    }
-}
-
-impl<'a> ops::BitOr<TokenBuilder<'a>> for TokenBuilder<'a> {
-    type Output = TokenBuilder<'a>;
-
-    fn bitor(self, rhs: TokenBuilder<'a>) -> TokenBuilder<'a> {
-        TokenBuilder::And(Box::new(self), Box::new(rhs))
+    fn bitor(self, rhs: Element<'a>) -> Element<'a> {
+        Element::Or(Box::new(self), Box::new(rhs))
     }
 }
 
 #[cfg(test)]
-mod regex_and_should {
-    use super::{Tok, TokenBuilder};
+mod regex_or_should {
+    use super::{Elem, Element};
 
     #[test]
-    fn make_and_char_a_char_b () {
-        let actual ='a'.tok() & 'b'.tok();
-        let expected = TokenBuilder::And(Box::new(TokenBuilder::Char('a')), Box::new(TokenBuilder::Char('b')));
+    fn make_or_char_a_char_b () {
+        let actual ='a'.elem() | 'b'.elem();
+        let expected = Element::Or(Box::new(Element::Char('a')), Box::new(Element::Char('b')));
 
         assert_eq!(expected, actual);
     }
 }
 
 #[allow(non_upper_case_globals)]
-pub const eof: TokenBuilder = TokenBuilder::Eof;
+pub const eof: Element = Element::Eof;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Position {
@@ -199,7 +222,7 @@ pub type Mapper<Token> = fn(String) -> Token;
 
 #[derive(Debug, PartialEq)]
 pub struct Tokenizer<'a, Token> {
-    builder: TokenBuilder<'a>,
+    builder: Element<'a>,
     mapper: Mapper<Token>
 }
 
@@ -209,7 +232,7 @@ pub enum Rule<'a, Token> {
     Multiple(Vec<Tokenizer<'a, Token>>)
 }
 
-impl<'a, Token> ops::Shr<Mapper<Token>> for TokenBuilder<'a> {
+impl<'a, Token> Shr<Mapper<Token>> for Element<'a> {
     type Output = Rule<'a, Token>;
     
     fn shr(self, rhs: Mapper<Token>) -> Self::Output {
@@ -220,7 +243,7 @@ impl<'a, Token> ops::Shr<Mapper<Token>> for TokenBuilder<'a> {
     }
 }
 
-impl<'a, Token> ops::BitOr<Rule<'a, Token>> for Rule<'a, Token> {
+impl<'a, Token> BitOr<Rule<'a, Token>> for Rule<'a, Token> {
     type Output = Rule<'a, Token>;
 
     fn bitor(self, rhs: Rule<'a, Token>) -> Rule<'a, Token> {
@@ -249,7 +272,7 @@ impl<'a, Token> ops::BitOr<Rule<'a, Token>> for Rule<'a, Token> {
 
 #[cfg(test)]
 mod tokenizer_should {
-    use super::{p, Rule, Tokenizer};
+    use super::{p, Rule, Tokenizer, Elem};
 
     #[derive(Debug, PartialEq)]
     enum Token {
@@ -267,7 +290,7 @@ mod tokenizer_should {
 
     #[test]
     fn make_multiple_rules() {
-        let identifier = p(|c| c.is_alphabetic()) & p(|c| c.is_alphanumeric()).rep0();
+        let identifier = vec![p(|c| c.is_alphabetic()), p(|c| c.is_alphanumeric()).rep0()].elem();
         let integer = p(|c| c.is_digit(10)).rep1();
 
         let actual
--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,5 @@
-use super::{Predicate, TokenBuilder, ParseError, Error, Tokenizer, Rule};
+use std::ops::Range;
+use super::{Predicate, Element, ParseError, Error, Tokenizer, Rule};
 use super::stream::CharStream;
 
 fn parse_char(char: char, buffer: &mut String, stream: &mut dyn CharStream) -> Result<(), ParseError> {
@@ -58,12 +59,16 @@ fn parse_string(string: &str, buffer: &mut String, stream: &mut dyn CharStream) 
     Ok(())
 }
 
-fn parse_repeat(builder: &TokenBuilder, min: usize, max: Option<usize>, buffer: &mut String, stream: &mut dyn CharStream) -> Result<(), ParseError> {
+fn parse_repeat(element: &Element, range: &Range<u32>, buffer: &mut String, stream: &mut dyn CharStream) -> Result<(), ParseError> {
+    if range.is_empty() {
+        return Ok(());
+    }
+    
     let buffer_length = buffer.len();
     stream.store_state();
 
-    for _ in 0..min {
-        if let Err(parse_error) = parse_into_buffer(buffer, &builder, stream) {
+    for _ in 0..range.start {
+        if let Err(parse_error) = parse_into_buffer(buffer, &element, stream) {
             buffer.truncate(buffer_length);
             stream.restore_state();
 
@@ -71,48 +76,39 @@ fn parse_repeat(builder: &TokenBuilder, min: usize, max: Option<usize>, buffer: 
         }
     }
 
-    if let Some(max) = max {
-        for _ in (max - min)..max {
-            if let Err(parse_error) = parse_into_buffer(buffer, &builder, stream) {
-                buffer.truncate(buffer_length);
-                stream.restore_state();
-                
-                return Err(parse_error);
-            }
+    for _ in range.start..range.end {
+        if let Err(_) = parse_into_buffer(buffer, &element, stream) {
+            break;
         }
-        } else {
-        while let Ok(()) = parse_into_buffer(buffer, &builder, stream) { }
     }
 
     stream.discard_state();
     Ok(())
 }
 
-fn parse_and(builder1: &TokenBuilder, builder2: &TokenBuilder, buffer: &mut String, stream: &mut dyn CharStream) -> Result<(), ParseError> {
+fn parse_sequence(elements: &[Box<Element>], buffer: &mut String, stream: &mut dyn CharStream) -> Result<(), ParseError> {
     let buffer_length = buffer.len();
     stream.store_state();
 
-    if let Err(parse_error) = parse_into_buffer(buffer, builder1, stream) {
-        return Err(parse_error);
+    for element in elements {
+        if let Err(parse_error) = parse_into_buffer(buffer, element, stream) {
+            buffer.truncate(buffer_length);
+            stream.restore_state();
+    
+            return Err(parse_error);
+        }
     }
 
-    if let Err(parse_error) = parse_into_buffer(buffer, builder2, stream) {
-        buffer.truncate(buffer_length);
-        stream.restore_state();
-
-        Err(parse_error)
-    } else {
-        stream.discard_state();
-        Ok(())
-    }
+    stream.discard_state();
+    Ok(())
 }
 
-fn parse_or(builder1: &TokenBuilder, builder2: &TokenBuilder, buffer: &mut String, stream: &mut dyn CharStream) -> Result<(), ParseError> {
+fn parse_or(element1: &Element, element2: &Element, buffer: &mut String, stream: &mut dyn CharStream) -> Result<(), ParseError> {
     let buffer_length = buffer.len();
     stream.store_state();
 
-    if let Err(_) = parse_into_buffer(buffer, builder1, stream) {
-        if let Err(parse_error) = parse_into_buffer(buffer, builder2, stream) {
+    if let Err(_) = parse_into_buffer(buffer, element1, stream) {
+        if let Err(parse_error) = parse_into_buffer(buffer, element2, stream) {
             buffer.truncate(buffer_length);
             stream.restore_state();
     
@@ -132,22 +128,22 @@ fn parse_eof(stream: &mut dyn CharStream) -> Result<(), ParseError> {
     }
 }
 
-fn parse_into_buffer(buffer: &mut String, builder: &TokenBuilder, stream: &mut dyn CharStream) -> Result<(), ParseError> {
-    match builder {
-        TokenBuilder::Char(char) => parse_char(*char, buffer, stream),
-        TokenBuilder::Predicate(predicate) => parse_predicate(predicate, buffer, stream),
-        TokenBuilder::String(string) => parse_string(*string, buffer, stream),
-        TokenBuilder::Repeat(regex, min, max) => parse_repeat(regex.as_ref(), *min, *max, buffer, stream),
-        TokenBuilder::And(regex1, regex2) => parse_and(regex1.as_ref(), regex2.as_ref(), buffer, stream),
-        TokenBuilder::Or(regex1, regex2) => parse_or(regex1.as_ref(), regex2.as_ref(), buffer, stream),
-        TokenBuilder::Eof => parse_eof(stream)
+fn parse_into_buffer(buffer: &mut String, element: &Element, stream: &mut dyn CharStream) -> Result<(), ParseError> {
+    match element {
+        Element::Char(char) => parse_char(*char, buffer, stream),
+        Element::Predicate(predicate) => parse_predicate(predicate, buffer, stream),
+        Element::String(string) => parse_string(*string, buffer, stream),
+        Element::Repeat(element, range) => parse_repeat(element.as_ref(), range, buffer, stream),
+        Element::Sequence(elements) => parse_sequence(elements, buffer, stream),
+        Element::Or(element1, element2) => parse_or(element1.as_ref(), element2.as_ref(), buffer, stream),
+        Element::Eof => parse_eof(stream)
     }
 }
 
-pub fn parse_builder(builder: &TokenBuilder, stream: &mut dyn CharStream) -> Result<String, ParseError> {
+pub fn parse_element(element: &Element, stream: &mut dyn CharStream) -> Result<String, ParseError> {
     let mut buffer = String::new();
     
-    if let Err(parse_error) = parse_into_buffer(&mut buffer, builder, stream) {
+    if let Err(parse_error) = parse_into_buffer(&mut buffer, element, stream) {
         Err(parse_error)
     } else {
         Ok(buffer)
@@ -155,19 +151,19 @@ pub fn parse_builder(builder: &TokenBuilder, stream: &mut dyn CharStream) -> Res
 }
 
 #[cfg(test)]
-mod parse_builder_should {
+mod parse_element_should {
     use crate::stream::CharStream;
 
-    use super::parse_builder;
-    use super::super::{Tok, p, eof};
+    use super::parse_element;
+    use super::super::{Elem, p, eof};
     use super::super::stream::StringCharStream;
 
     #[test]
     fn parse_abc_when_regex_is_abc_eof() {
         let mut stream = StringCharStream::new("abc");
-        let regex = 'a'.tok() & 'b'.tok() & 'c'.tok() & eof;
+        let regex = vec!['a'.elem(), 'b'.elem(), 'c'.elem(), eof].elem();
 
-        assert_eq!(Ok("abc".to_string()), parse_builder(&regex, &mut stream));
+        assert_eq!(Ok("abc".to_string()), parse_element(&regex, &mut stream));
     }
 
     #[test]
@@ -175,13 +171,13 @@ mod parse_builder_should {
         let mut stream = StringCharStream::new("1234567.89");
         let digits1 = p(|c: char| c.is_digit(10)).rep1();
 
-        assert_eq!(Ok("1234567".to_string()), parse_builder(&digits1, &mut stream));
+        assert_eq!(Ok("1234567".to_string()), parse_element(&digits1, &mut stream));
         assert_eq!(Some('.'), stream.peek());
     }
 }
 
 fn parse_tokenizer<'a, Token>(tokenizer: &Tokenizer<'a, Token>, stream: &mut dyn CharStream) -> Result<Token, ParseError> {
-    parse_builder(&tokenizer.builder, stream).map(tokenizer.mapper)
+    parse_element(&tokenizer.builder, stream).map(tokenizer.mapper)
 }
 
 fn parse_tokenizers<'a, Token>(tokenizers: &Vec<Tokenizer<'a, Token>>, stream: &mut dyn CharStream) -> Result<Token, ParseError> {
@@ -206,7 +202,7 @@ pub fn parse_rule<'a, Token>(rule: &Rule<'a, Token>, stream: &mut dyn CharStream
 #[cfg(test)]
 mod parse_rule_should {
     use super::parse_rule;
-    use super::super::{p, Position, Error, ParseError};
+    use super::super::{p, Position, Error, ParseError, Elem};
     use super::super::stream::StringCharStream;
 
     #[derive(Debug, PartialEq)]
@@ -226,7 +222,7 @@ mod parse_rule_should {
     #[test]
     fn recognize_identifer() {
         let mut stream = StringCharStream::new("abc");
-        let identifier = p(|c| c.is_alphabetic()) & p(|c| c.is_alphanumeric()).rep0();
+        let identifier = vec![p(|c| c.is_alphabetic()), p(|c| c.is_alphanumeric()).rep0()].elem();
         let integer = p(|c| c.is_digit(10)).rep1();
 
         let rule
@@ -241,7 +237,7 @@ mod parse_rule_should {
     #[test]
     fn recognize_integer() {
         let mut stream = StringCharStream::new("123");
-        let identifier = p(|c| c.is_alphabetic()) & p(|c| c.is_alphanumeric()).rep0();
+        let identifier = vec![p(|c| c.is_alphabetic()), p(|c| c.is_alphanumeric()).rep0()].elem();
         let integer = p(|c| c.is_digit(10)).rep1();
 
         let rule
@@ -256,7 +252,7 @@ mod parse_rule_should {
     #[test]
     fn do_not_recognize_plus_sign() {
         let mut stream = StringCharStream::new("+123");
-        let identifier = p(|c| c.is_alphabetic()) & p(|c| c.is_alphanumeric()).rep0();
+        let identifier = vec![p(|c| c.is_alphabetic()), p(|c| c.is_alphanumeric()).rep0()].elem();
         let integer = p(|c| c.is_digit(10)).rep1();
 
         let rule
